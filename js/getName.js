949849308e530bee3714f45e1a35aaf4905af2e9
leyla li <leylali@qq.com>
2019-06-25 10:57:14 -0700

    put name into local storage and print out different welcome words

--- js/getName.js
+++ js/getName.js
@@ -1,14 +1,33 @@
 'use strict';
 
 var userName = prompt('What is your name?');
-
+var nameList = [];
+var welcomeWords = document.getElementById('welcomeWords');
+checkLocalStorage();
+checknameList();
+addToLocalStorage('nameList', nameList);
 //We need to put the user names into local storage
 //if there is the same name on LS then on the front page it says welcome back xxx!
 //if there is no same names on LS then on the front page it says Hi xx!
 
-var welcomeWords = document.getElementById('welcomeWords');
-if(userName !== null){
-  welcomeWords.textContent = `${userName}, Welcome To The Game!`;
-}else{
-  welcomeWords.textContent = 'Welcome to the game!';
+//see if the name is in the  LS
+function checkLocalStorage(){
+  if(localStorage.nameList){
+    nameList = getDataFromLocalStorage("nameList");
+// }else{
+//   nameList.push(userName);
+ }
+}
+
+//see if the name is in the nameList
+function checknameList(){
+  userName = userName.toUpperCase();
+  if(nameList.includes(userName)){
+    welcomeWords.textContent = userName + ', WELCOME BACK TO THE GAME!';
+  }else if(userName === null){
+    welcomeWords.textContent = 'WELCOME TO THE GAME!';
+  }else{
+    welcomeWords.textContent = `${userName}, WELCOME TO THE GAME!`;
+    nameList.push(userName);
+  }
 }
--- js/utils.js
+++ js/utils.js
@@ -12,9 +12,17 @@ function distance(x1, y1, x2, y2) {
   const xDist = x2 - x1;
   const yDist = y2 - y1;
 
-  return Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2))
+  return Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2));
 }
 
-function randomNumberBetween(min, max) { 
+function randomNumberBetween(min, max) {
   return Math.random() * (max - min + 1) + min;
+}
+
+function addToLocalStorage(storageName,storageData){
+  localStorage.setItem(storageName,JSON.stringify(storageData));
+}
+
+function getDataFromLocalStorage(storageName){
+  return JSON.parse(localStorage.getItem(storageName));
 }
