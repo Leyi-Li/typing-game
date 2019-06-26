4c5db39219a97aaddfef19f65cf5884e779ee300
leyla li <leylali@qq.com>
2019-06-26 15:02:30 -0700

    created homepage buttons

--- js/endGameLinks.js
+++ js/endGameLinks.js
@@ -9,4 +9,8 @@ function homePageLink() {
   ctx.fillStyle = 'white';
   ctx.fillText('Play Again', 150, canvas.height/2);
   ctx.fillText('Home', 450, canvas.height/2);
+  ctx.rect(145,canvas.height/2-27,155, 40);
+  ctx.rect(445,canvas.height/2-27,90, 40);
+  ctx.strokeStyle = 'white';
+  ctx.stroke();
 }
