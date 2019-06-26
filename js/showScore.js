dd30f12b3f0f9935ba6b6f939eb355bd18ce82a0
Quang Nguyen <quangn2595@gmail.com>
2019-06-26 13:41:11 -0700

    minor changes

--- js/showScore.js
+++ js/showScore.js
@@ -1,9 +1,9 @@
 'use strict';
 
 function drawScore() {
-    var updateScore = requestAnimationFrame(drawScore);
-    ctx.font = '25px Gadget';
-    ctx.fillStyle = 'white';
-    ctx.fillText('Score: '+ score, 8, 25);
-  }
+  var updateScore = requestAnimationFrame(drawScore);
+  ctx.font = '25px Gadget';
+  ctx.fillStyle = 'white';
+  ctx.fillText('Score: '+ score, 8, 25);
+}
 
