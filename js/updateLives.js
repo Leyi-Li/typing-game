d0fec6cc53a36e114832ddc5d74eb3abcbf05501
Quang Nguyen <quangn2595@gmail.com>
2019-06-28 10:46:02 -0700

    fixed lives

--- js/updateLives.js
+++ js/updateLives.js
@@ -5,12 +5,12 @@ function drawBox(){
   var location;
   if(lives === 2){
     location = document.getElementById(imgId[1]);
-    location.src='../img/Picture1.png';
+    location.src='./img/Picture1.png';
   }else if(lives === 1){
     location = document.getElementById(imgId[2]);
-    location.src='../img/Picture1.png';
+    location.src='./img/Picture1.png';
   }else if(lives ===0){
     location = document.getElementById(imgId[0]);
-    location.src='../img/Picture1.png';
+    location.src='./img/Picture1.png';
   }
 }
